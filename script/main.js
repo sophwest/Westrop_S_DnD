5a8736cb5b9fe74f3ea9048e05555f33dbfac98f
sophwest <s_westrop2@fanshaweonline.ca>
2023-02-03 11:25:30 -0500

    successfully did dnd

--- script/main.js
+++ script/main.js
@@ -5,9 +5,17 @@
 // let navButtons = document.querySelector("#buttonOne");
 
 // this is a 1 to many connecton to elements in the DOM
-let navButtons = document.querySelectorAll("#buttonHolder img"),
-theHeadLine = document.querySelector("#headLine h1"),
-puzzleBoard = document.querySelector(".puzzle-board");
+let navButtons = document.querySelectorAll('#buttonHolder img'),
+theHeadLine = document.querySelector('#headLine h1'),
+//collect all of the draggable pieces in the drag zone
+puzzlePieces = document.querySelectorAll ('.puzzle-pieces img'),
+//collect ALL of the drop zone elements
+dropZones = document.querySelectorAll(".drop-zone"),
+puzzleBoard = document.querySelector('.puzzle-board'),
+tempLink = document.querySelector('a'),
+//set up a global variable to store a reference to the drag piece
+//i need to know this later when i drop it on a zone
+draggedPiece;
 
 // functions go in the middle
 // these are the "actions" that should happen
@@ -26,8 +34,43 @@ function changeBGImage() {
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
+function handleStartDrag () { 
+	console.log('started draggin a piece!', this); 
+	//store the element I am currently dragging in that global draggedPiece variable
+	draggedPiece = this;
+}
+
+function handleDragOver(e) { 
+	e.preventDefault();
+	console.log('dragging over me!'); 
+}
+
+function handleDrop(e) {
+	//block the default bhevaiour
+	e.preventDefault();
+	//and then do whatever you want 
+	console.log('dropped on me!'); 
+	e.target.appendChild(draggedPiece);
+}
+
+function blockDefaultBehaviour(e) { 
+	//dont let the default behaviour of certain elements happen- block it
+	//e is short for event, in this case the nav event
+	e.preventDefault();
+}
+
 // event handling at the bottom - how is the user going to interact w the elements/controls you provide?
 // this is how things react when you interact with them
 
 // process a collection of elements and add an event handler to each
-navButtons.forEach(button => button.addEventListener("click", changeBGImage));
+navButtons.forEach(button => button.addEventListener('click', changeBGImage));
+
+//add the drag start handler to all of the puzzle pieces
+puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
+
+//add the dragover handling to the drop zones
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
+
+//temp handling
+tempLink.addEventListener('click', blockDefaultBehaviour);
